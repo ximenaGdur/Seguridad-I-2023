0bba270a42f3ce6c7dfceced0300bca7fe61304a
ximenaGdur <68168666+ximenaGdur@users.noreply.github.com>
2023-09-10 10:38:52 -0600

    Creating basic structure for PHP code
    Co-authored-by: mj-chuz and kvasquezj

---
+++ auth/public/register-form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+  <title>Registro de usuarios</title>
+</head>
+
+<body>
+  <main>
+    <form action="register.php" method="post">
+      <h1>Ingrese los siguientes datos:</h1>
+      <div>
+        <label for="email">Usuario:</label>
+        <input type="email" name="email" id="email">
+      </div>
+      <div>
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password">
+      </div>
+      <button type="submit">Registrar</button>
+    </form>
+  </main>
+</body>
+
+</html>
