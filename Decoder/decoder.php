462d646d1500300ca5c7be7ec2841d11505057fe
ximenaGdur <68168666+ximenaGdur@users.noreply.github.com>
2023-09-19 19:50:55 -0600

    Quitando impresiones

--- Decoder/decoder.php
+++ Decoder/decoder.php
@@ -16,21 +16,18 @@ function creatingHashes($existingPasswords) {
   // 1 word password
   foreach ($existingPasswords as $existingPassword1) {
     $hashedPassword = hash('sha256', $existingPassword1);
-    echo 'contraseña existente: [' . $existingPassword1 . '] con hash: ' . $hashedPassword . '<br>';
     // Adding 1 word password to array
     array_push($hashesArray, trim($hashedPassword));
     // 2 word password
     foreach ($existingPasswords as $existingPassword2) {
       $combinedPassword1 = $existingPassword1 . $existingPassword2;
       $hashedPassword2 = hash('sha256', $combinedPassword1);
-      echo 'contraseña existente: [' . $combinedPassword1 . '] con hash: ' . $hashedPassword2 . '<br>';
       // Adding 2 words password to array
       array_push($hashesArray, trim($hashedPassword2));
       // 3 word password
       foreach ($existingPasswords as $existingPassword3) {
         $combinedPassword2 = $existingPassword1 . $existingPassword2 . $existingPassword3;
         $hashedPassword3 = hash('sha256', $combinedPassword2);
-        echo 'contraseña existente: [' . $combinedPassword2 . '] con hash: ' . $hashedPassword3 . '<br>';
         // Adding 2 words password to array
         array_push($hashesArray, trim($hashedPassword3));
       }
@@ -55,12 +52,10 @@ function readPasswords($fileName) {
   // Abre el archivo TXT
   $file = fopen($fileName, "r");
   
-  echo 'Imprimiendo contenidos de archivo txt </br>';
   // Itera por todas las líneas del archivo
   while (($linea = fgets($file)) !== false) {
       // Agrega al arreglo la palabras
       array_push($passwords, trim($linea));
-      echo '[' . trim($linea) . ']</br>';
   }
 
   // Cierra el archivo TXT
@@ -78,14 +73,11 @@ function readUsers($fileName) {
       // Initialize an empty array to store the passwords
       $passwords = array();
 
-      echo 'Imprimiendo contenidos de archivo csv </br>';
       // Read each line of the CSV file
       while (($data = fgetcsv($fileOpened)) !== false) {
           if(count($data) >= 2){
               $username = $data[0];
               array_push($passwords, trim($data[1]));
-
-              echo 'Usuario: [' . trim($username) . '], Contraseña: [' . trim($data[1]) . ']<br>';
           }
       }
       // Close the CSV file
@@ -97,15 +89,13 @@ function readUsers($fileName) {
 }
 
 function comparingPasswords($userPasswords, $hashedExistingPasswords) {
-  echo '<br><br>COMPARANDO CONTRASEÑAS<br>';
+  echo '<h4>Comparando hashes:<h4>';
   // Iterating through user passwords
   foreach ($userPasswords as $userPassword) {
     foreach ($hashedExistingPasswords as $hashedPassword) {
       // Check the flag after the inner loop
       if ($hashedPassword == $userPassword) {
-        echo 'EXITO: ' . $hashedPassword . ' == ' . $userPassword . '<br>';
-      } else {
-        echo 'FALLO: ' . $hashedPassword . ' != ' . $userPassword . '<br>';
+        echo 'Exito: ' . $hashedPassword . ' == ' . $userPassword . '<br>';
       }
     }
   }
