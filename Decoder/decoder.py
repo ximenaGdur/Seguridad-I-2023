00a3ea99f85b46ad9689c565091479a47929a3bf
Karen Vásquez <87218104+KVasquezJ@users.noreply.github.com>
2023-09-25 21:33:22 -0600

    hilos

--- Decoder/decoder.py
+++ Decoder/decoder.py
@@ -1,6 +1,7 @@
 import csv
 from hashlib import sha256
-
+import threading
+from concurrent.futures import ThreadPoolExecutor
 def read_passwords(file_name):
   passwords = []
   try:
@@ -25,31 +26,52 @@ def read_users(file_name):
     print("File could not be opened")
   return passwords
 
-def creating_hashes(existing_passwords):
-  hashes_map = {}
-  for password1 in existing_passwords:
+def creating_hashes(existing_passwords,start, end, hashes_map):
+ 
+  for i in range(start, end):
+    password1=existing_passwords[i]
+    print(i)
     hashed_password = sha256(password1.encode('utf-8')).hexdigest()
     hashes_map[password1] = hashed_password.strip()
-    for password2 in existing_passwords:
+    for j in range(len(existing_passwords)):
+      password2 = existing_passwords[j]
       combined_password1 = password1 + password2
       hashed_password2 = sha256(combined_password1.encode('utf-8')).hexdigest()
       hashes_map[password2] = hashed_password2.strip()
-      for password3 in existing_passwords:
-        combined_password2 = password1 + password2 + password3
-        hashed_password3 = sha256(combined_password2.encode('utf-8')).hexdigest()
-        hashes_map[password3] = hashed_password3.strip()
+      for k in range(len(existing_passwords)):
+          password3 = existing_passwords[k]
+          combined_password2 = password1 + password2 + password3
+          hashed_password3 = sha256(combined_password2.encode('utf-8')).hexdigest()
+          hashes_map[password3] = hashed_password3.strip()
   return hashes_map
 
 def comparing_passwords(user_passwords, hashed_passwords):
   print('Comparing hashes:')
   for username in user_passwords.keys():
+    print(username)
     for password in hashed_passwords.keys():
       if hashed_passwords[password] == user_passwords[username]:
         print(f'Success for {username} with password {password}:')
         print(f'{hashed_passwords[password]} == {user_passwords[username]}\n')
 def main():
+  num_threads = 5
   existing_passwords = read_passwords('passwords.txt')
+  batch_size = len(existing_passwords) // num_threads
   user_passwords = read_users('users.csv')
+  result_dict = {}
+  with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = []
+
+        for i in range(num_threads):
+            start = i * batch_size
+            end = start + batch_size if i < num_threads - 1 else len(existing_passwords)
+            future = executor.submit(creating_hashes, existing_passwords, start, end, result_dict)
+            futures.append(future)
+
+        # Espera a que todos los hilos terminen
+        for future in futures:
+            future.result()
+  print("Fin") 
   hashed_passwords = creating_hashes(existing_passwords)
   comparing_passwords(user_passwords, hashed_passwords)
 
